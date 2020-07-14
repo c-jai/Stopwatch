d26e023e36b7d6f15caef3875460b9552c33f0dd
Jai <jai.chhatre@gmail.com>
2020-07-13 23:40:32 -0700

    Updates

---
+++ precache-manifest.033ce8c44a0f3e1009bdf66f098920e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b51e9c5aff2b794a9ec9348e8c51420",
+    "url": "/Stopwatch/index.html"
+  },
+  {
+    "revision": "1ff57f88d7001a54fe48",
+    "url": "/Stopwatch/static/css/main.d544d140.chunk.css"
+  },
+  {
+    "revision": "a5ff9f07d67a97a6236c",
+    "url": "/Stopwatch/static/js/2.18d68d4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stopwatch/static/js/2.18d68d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff57f88d7001a54fe48",
+    "url": "/Stopwatch/static/js/main.4f3734a8.chunk.js"
+  },
+  {
+    "revision": "b018cdbb21fcc2e90e5d",
+    "url": "/Stopwatch/static/js/runtime-main.ce0443d9.js"
+  }
+]);
